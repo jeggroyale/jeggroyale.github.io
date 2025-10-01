240b7f97a1e21a44f3ba2216634fa8b3bbc2f473
Alfie Conroy <alfiepconroy@icloud.com>
2025-10-01 18:37:15 +0100

    Create cards.js

---
+++ src/cards.js
@@ -0,0 +1,340 @@
+// Full list of official Clash Royale cards as of October 2025
+export const cards = [
+  {
+    name: "Knight",
+    id: 26000000,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000000.png"
+  },
+  {
+    name: "Archers",
+    id: 26000001,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000001.png"
+  },
+  {
+    name: "Goblins",
+    id: 26000002,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000002.png"
+  },
+  {
+    name: "Giant",
+    id: 26000003,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000003.png"
+  },
+  {
+    name: "P.E.K.K.A",
+    id: 26000004,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000004.png"
+  },
+  {
+    name: "Minions",
+    id: 26000005,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000005.png"
+  },
+  {
+    name: "Balloon",
+    id: 26000006,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000006.png"
+  },
+  {
+    name: "Witch",
+    id: 26000007,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000007.png"
+  },
+  {
+    name: "Barbarians",
+    id: 26000008,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000008.png"
+  },
+  {
+    name: "Golem",
+    id: 26000009,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000009.png"
+  },
+  {
+    name: "Skeletons",
+    id: 26000010,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000010.png"
+  },
+  {
+    name: "Valkyrie",
+    id: 26000011,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000011.png"
+  },
+  {
+    name: "Skeleton Army",
+    id: 26000012,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000012.png"
+  },
+  {
+    name: "Bomber",
+    id: 26000013,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000013.png"
+  },
+  {
+    name: "Musketeer",
+    id: 26000014,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000014.png"
+  },
+  {
+    name: "Baby Dragon",
+    id: 26000015,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000015.png"
+  },
+  {
+    name: "Prince",
+    id: 26000016,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000016.png"
+  },
+  {
+    name: "Wizard",
+    id: 26000017,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000017.png"
+  },
+  {
+    name: "Mini P.E.K.K.A",
+    id: 26000018,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000018.png"
+  },
+  {
+    name: "Spear Goblins",
+    id: 26000019,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000019.png"
+  },
+  {
+    name: "Giant Skeleton",
+    id: 26000020,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000020.png"
+  },
+  {
+    name: "Hog Rider",
+    id: 26000021,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000021.png"
+  },
+  {
+    name: "Minion Horde",
+    id: 26000022,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000022.png"
+  },
+  {
+    name: "Ice Wizard",
+    id: 26000023,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000023.png"
+  },
+  {
+    name: "Royal Giant",
+    id: 26000024,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000024.png"
+  },
+  {
+    name: "Guards",
+    id: 26000025,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000025.png"
+  },
+  {
+    name: "Princess",
+    id: 26000026,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000026.png"
+  },
+  {
+    name: "Dark Prince",
+    id: 26000027,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000027.png"
+  },
+  {
+    name: "Three Musketeers",
+    id: 26000028,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000028.png"
+  },
+  {
+    name: "Lava Hound",
+    id: 26000029,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000029.png"
+  },
+  {
+    name: "Ice Spirit",
+    id: 26000030,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000030.png"
+  },
+  {
+    name: "Fire Spirits",
+    id: 26000031,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000031.png"
+  },
+  {
+    name: "Miner",
+    id: 26000032,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000032.png"
+  },
+  {
+    name: "Sparky",
+    id: 26000033,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000033.png"
+  },
+  {
+    name: "Bowler",
+    id: 26000034,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000034.png"
+  },
+  {
+    name: "Inferno Dragon",
+    id: 26000035,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000035.png"
+  },
+  {
+    name: "Battle Ram",
+    id: 26000036,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000036.png"
+  },
+  {
+    name: "Ice Golem",
+    id: 26000037,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000037.png"
+  },
+  {
+    name: "Dart Goblin",
+    id: 26000038,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000038.png"
+  },
+  {
+    name: "Goblin Gang",
+    id: 26000039,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000039.png"
+  },
+  {
+    name: "Electro Wizard",
+    id: 26000040,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000040.png"
+  },
+  {
+    name: "Elite Barbarians",
+    id: 26000041,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000041.png"
+  },
+  {
+    name: "Executioner",
+    id: 26000042,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000042.png"
+  },
+  {
+    name: "Hunter",
+    id: 26000043,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000043.png"
+  },
+  {
+    name: "Bandit",
+    id: 26000044,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000044.png"
+  },
+  {
+    name: "Royal Recruits",
+    id: 26000045,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000045.png"
+  },
+  {
+    name: "Royal Ghost",
+    id: 26000046,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000046.png"
+  },
+  {
+    name: "Ram Rider",
+    id: 26000047,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000047.png"
+  },
+  {
+    name: "Zappies",
+    id: 26000048,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000048.png"
+  },
+  {
+    name: "Rascals",
+    id: 26000049,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000049.png"
+  },
+  {
+    name: "Cannon Cart",
+    id: 26000050,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000050.png"
+  },
+  {
+    name: "Mega Minion",
+    id: 26000039,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000039.png"
+  },
+  {
+    name: "Skeleton Barrel",
+    id: 26000052,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000052.png"
+  },
+  {
+    name: "Flying Machine",
+    id: 26000053,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000053.png"
+  },
+  {
+    name: "Wall Breakers",
+    id: 26000054,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000054.png"
+  },
+  {
+    name: "Royal Hogs",
+    id: 26000055,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000055.png"
+  },
+  {
+    name: "Goblin Giant",
+    id: 26000056,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000056.png"
+  },
+  {
+    name: "Fisherman",
+    id: 26000057,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000057.png"
+  },
+  {
+    name: "Magic Archer",
+    id: 26000058,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000058.png"
+  },
+  {
+    name: "Electro Dragon",
+    id: 26000059,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000059.png"
+  },
+  {
+    name: "Mother Witch",
+    id: 26000060,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000060.png"
+  },
+  {
+    name: "Skeleton King",
+    id: 26000061,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000061.png"
+  },
+  {
+    name: "Golden Knight",
+    id: 26000062,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000062.png"
+  },
+  {
+    name: "Archer Queen",
+    id: 26000063,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000063.png"
+  },
+  {
+    name: "Mighty Miner",
+    id: 26000064,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000064.png"
+  },
+  {
+    name: "Monk",
+    id: 26000065,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000065.png"
+  },
+  {
+    name: "Phoenix",
+    id: 26000066,
+    image: "https://royaleapi.github.io/cr-api-assets/cards/26000066.png"
+  },
+  // Add any new cards as they are released
+  // For brevity, buildings and spells are omitted but you should include them in production!
+];
